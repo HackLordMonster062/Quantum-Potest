265553a4dff33d9a1f1c2b8088e4ba8d01770575
HackLordMonster062 <hackmonster062@gmail.com>
2024-08-12 14:23:32 +0300

    Tunneling
    Tunnellable walls have a minimum energy and objects with that amount of energy can phase through

--- Quantum Stuff/Assets/Scripts/Tunnelable.cs
+++ Quantum Stuff/Assets/Scripts/Tunnelable.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 
 public class Tunnelable : MonoBehaviour {
-
+	[SerializeField] float minimumEnergy;
 
 	private void OnCollisionEnter(Collision collision) {
 		Excitable other = collision.collider.GetComponent<Excitable>();
 
-		if (other != null) {
-			print(other.gameObject);
+		if (other != null && other.Energy >= minimumEnergy) {
+			Vector3 relativePosition = other.transform.position - transform.position;
+
+			Vector3 newPosition = Vector3.Reflect(relativePosition, transform.right) + transform.position;
+
+			other.transform.position = newPosition;
 		}
 	}
 }
