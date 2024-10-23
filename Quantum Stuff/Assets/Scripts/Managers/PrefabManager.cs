2dde134574132f2c1a867d8c4e61755beec74e26
HackLordMonster062 <hackmonster062@gmail.com>
2024-10-23 11:15:25 +0300

    Prefab management

--- Quantum Stuff/Assets/Scripts/Managers/PrefabManager.cs
+++ Quantum Stuff/Assets/Scripts/Managers/PrefabManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class PrefabManager : Singleton<PrefabManager> {
 	[SerializeField] ParticlePM particles;
 	public ParticlePM Particles { get { return particles; } }
+
+	[SerializeField] DevicePM devices;
+	public DevicePM Devices { get { return devices; } }
 }
 
 [Serializable]
@@ -13,4 +16,31 @@ public class ParticlePM {
 
 	[SerializeField] GameObject emitter;
 	public GameObject Emitter { get { return emitter; } }
+
+	[SerializeField] GameObject massiveParticle;
+	public GameObject MassiveParticle { get { return massiveParticle; } }
+
+	[SerializeField] GameObject coloredKey;
+	public GameObject ColoredKey { get { return coloredKey; } }
+
+	[SerializeField] GameObject activatorParticle;
+	public GameObject ActivatorParticle { get { return activatorParticle; } }
+}
+
+[Serializable]
+public class DevicePM {
+	[SerializeField] GameObject anchor;
+	public GameObject Anchor { get { return anchor; } }
+
+	[SerializeField] GameObject rotator;
+	public GameObject Rotator { get { return rotator; } }
+
+	[SerializeField] GameObject spinner;
+	public GameObject Spinner { get { return spinner; } }
+
+	[SerializeField] GameObject photonShooter;
+	public GameObject PhotonShooter { get { return photonShooter; } }
+
+	[SerializeField] GameObject coloredDoor;
+	public GameObject ColoredDoor { get { return coloredDoor; } }
 }
