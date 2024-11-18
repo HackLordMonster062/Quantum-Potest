15e2210d129dcf269365ff180869554430f972e2
HackLordMonster062 <hackmonster062@gmail.com>
2024-11-18 21:27:57 +0200

    More large level work

--- Quantum Stuff/Assets/Scripts/Devices/PhotonShooter.cs
+++ Quantum Stuff/Assets/Scripts/Devices/PhotonShooter.cs
@@ -1,11 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class PhotonShooter : Activatable {
     [SerializeField] float shootingPointDistance;
+	[SerializeField] bool isStream;
+
+	private void Start() {
+		if (isStream) StartCoroutine(PhotonStream());
+	}
 
 	public override void Activate() {
 		Vector3 shootingPoint = transform.position + transform.forward * shootingPointDistance;
 
 		Instantiate(PrefabManager.instance.Particles.Photon, shootingPoint, transform.rotation);
+	}
+
+	IEnumerator PhotonStream() {
+		while (true) {
+			yield return new WaitForSeconds(PhysicsManager.instance.RelaxtationTime);
+
+			Activate();
+		}
 	}
 }
